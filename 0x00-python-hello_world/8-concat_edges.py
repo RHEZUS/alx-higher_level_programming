24cf19fe2dbbc7586715507766d7c6fec2207f88
RHEZUS <ludivinseunda2@gmail.com>
2023-11-27 13:29:47 +0300

    solve the mandatory tasks

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[39:66]:s} {str[107:111]:s} {str[:6]:s}")
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
