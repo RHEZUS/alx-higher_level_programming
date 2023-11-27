f0a9c0b264fe171f1a105853528bcf718f17c533
RHEZUS <ludivinseunda2@gmail.com>
2023-11-27 14:13:12 +0300

    solve the mandatory tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,9 +11,9 @@ int check_cycle(listint_t *list)
 	listint_t *tortoise = list;
 	listint_t *hare = list;
 
-	if (list == NULL && list->next == NULL)
+	if (!list)
 		return(0);
-	while (hare != NULL && hare->next != NULL)
+	while (hare && tortoise && hare->next != NULL)
 	{
 		tortoise = tortoise->next;
 		hare = hare->next->next;
