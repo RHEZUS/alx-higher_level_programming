71e9713e70a156de464faac02fc17ec2aa3084ad
RHEZUS <ludivinseunda2@gmail.com>
2023-11-30 14:33:05 +0300

    solve the mandatory tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, div, mul, sub
+
+length = len(sys.argv) - 1
+
+if length != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+
+match sys.argv[2]:
+    case "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    case "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    case "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    case "/":
+        print("{} * {} = {}".format(a, b, div(a, b)))
+    case _:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
