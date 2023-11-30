0ab40cc9deb9ca3b5acf3257be4636f94ffd46e8
RHEZUS <ludivinseunda2@gmail.com>
2023-11-30 09:33:15 -0800

    solve task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
 
 index = sorted(dir(hidden_4))
 for index, item in enumerate(index):
-    if item[:2] == '__':
+    if item[:2] != '__':
         print("{}".format(item))
