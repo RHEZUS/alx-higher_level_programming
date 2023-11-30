23105e62ff06d4c4c9ee98bfa5a6fb1c2ffa1b38
RHEZUS <ludivinseunda2@gmail.com>
2023-11-30 18:01:11 +0300

    solve the mandatory tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,38 +8,13 @@ if len(sys.argv) - 1 != 3:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     sys.exit(1)
 
+operations = {'+': add, '-': sub, '*': mul, '/': div}
+
+if sys.argv[2] not in list(operations.keys()):
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
 a = int(sys.argv[1])
 b = int(sys.argv[3])
 
-match sys.argv[2]:
-    case '+':
-        print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
-    case '-':
-        print("{} {} {} = {}".format(a, sys.argv[2],  b, sub(a, b)))
-    case "*":
-        print("{} {} {} = {}".format(a, sys.argv[2],  b, mul(a, b)))
-    case '/':
-        print("{} {} {} = {}".format(a, sys.argv[2],  b, div(a, b)))
-    case _:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-'''
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
-'''
+print("{} {} {} = {}".format(a, sys.argv[2], b, operations[sys.argv[2]](a, b)))
