00de54cc1fbf47b06129982f7fab0f0d433944a7
RHEZUS <ludivinseunda2@gmail.com>
2023-11-30 17:51:28 +0300

    solve the mandatory tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, div, mul, sub
 
+# checks the number of arguments
+if len(sys.argv) - 1 != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+
+match sys.argv[2]:
+    case '+':
+        print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
+    case '-':
+        print("{} {} {} = {}".format(a, sys.argv[2],  b, sub(a, b)))
+    case "*":
+        print("{} {} {} = {}".format(a, sys.argv[2],  b, mul(a, b)))
+    case '/':
+        print("{} {} {} = {}".format(a, sys.argv[2],  b, div(a, b)))
+    case _:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+'''
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
@@ -17,3 +42,4 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+'''
