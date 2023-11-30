e9e411ab37c3b71ac6226609bd2071180973d80f
RHEZUS <ludivinseunda2@gmail.com>
2023-11-30 18:38:28 +0300

    solve the mandatory tasks

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-for item in dir(hidden_4):
-    if item[0] != '_' and item[1] != '_':
+for item in sorted(dir(hidden_4)):
+    if item[:2] == '__':
         print(item)
