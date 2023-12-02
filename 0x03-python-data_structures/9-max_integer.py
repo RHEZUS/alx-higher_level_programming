e031b9166356f1e76114b1b42280b1bf76e1a65a
RHEZUS <ludivinseunda2@gmail.com>
2023-12-02 15:46:26 +0300

    solve the mandatory tasks

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    big = my_list[1]
+
+    for i in my_list:
+        if i > big:
+            big = i
+    return big
