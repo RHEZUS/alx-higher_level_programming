c8daecd2c1bb96df9d49769c7bcda1c950ec62f8
RHEZUS <ludivinseunda2@gmail.com>
2023-12-02 14:58:30 +0300

    solve the mandatory tasks

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     new_list = ''
     for i in my_string:
-        new_list += "" if i == 'c' or i == 'C' else i     
+        new_list += "" if i == 'c' or i == 'C' else i
     return new_list
