74c31eaf990fd942d2400a4c65e70a4778af96e2
RHEZUS <ludivinseunda2@gmail.com>
2023-12-02 15:57:38 +0300

    solve the mandatory tasks

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+def divisible_by_2(my_list=[]):
+    result = []
+    result += my_list
+
+    for i in range(len(my_list)):
+        result[i] = i % 2 == 0
+
+    return result
