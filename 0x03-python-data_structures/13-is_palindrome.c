5955c1be99f8f621b582b051b6ec78c232dbac95
RHEZUS <ludivinseunda2@gmail.com>
2023-12-03 13:12:38 +0300

    solve the mandatory tasks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,51 @@
 #include "lists.h"
 
+/**
+ * is_palindrome - checks if a singly linked list
+ * is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palndrome
+ */
+
 int is_palindrome(listint_t **head)
 {
-    
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *stackTop = NULL;
+
+	/* Traverse the linked list and push elements onto the stack */
+	while (fast != NULL && fast->next != NULL)
+	{
+		/* Push the value onto the stack */
+		listint_t *newNode = malloc(sizeof(listint_t));
+
+		if (newNode == NULL)
+			return (0);
+
+		newNode->n = slow->n;
+		newNode->next = stackTop;
+		stackTop = newNode;
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	/* If the length of the linked list is odd, skip the middle element */
+	if (fast != NULL)
+		slow = slow->next;
+
+	/* Traverse the remaining linked list and compare with the*/
+	/* elements popped from the stack */
+	while (slow != NULL)
+	{
+		/* Pop the value from the stack */
+		if (stackTop == NULL || stackTop->n != slow->n)
+			return (0);
+		listint_t *temp = stackTop;
+
+		stackTop = stackTop->next;
+		free(temp);
+		slow = slow->next;
+	}
+	return (1);
 }
