9e65f4635339b8479217ad229ea78504c06383c2
RHEZUS <ludivinseunda2@gmail.com>
2023-12-03 13:33:13 +0300

    solve the mandatory tasks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,27 +11,24 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *tortoise = *head, *hare = *head, *stack_top = NULL;
+	listint_t *temp = NULL, *new_node;
 
 	/* Traverse the linked list and push elements onto the stack */
 	while (hare != NULL && hare->next != NULL)
 	{
 		/* Push the value onto the stack */
-		listint_t *new_node = malloc(sizeof(listint_t));
-
+		new_node = malloc(sizeof(listint_t));
 		if (new_node == NULL)
 			return (0);
-
 		new_node->n = tortoise->n;
 		new_node->next = stack_top;
 		stack_top = new_node;
-
 		tortoise = tortoise->next;
 		hare = hare->next->next;
 	}
 	/* If the length of the linked list is odd, skip the middle element */
 	if (hare != NULL)
 		tortoise = tortoise->next;
-
 	/* Traverse the remaining linked list and compare with the*/
 	/* elements popped from the stack */
 	while (tortoise != NULL)
@@ -39,8 +36,7 @@ int is_palindrome(listint_t **head)
 		/* Pop the value from the stack */
 		if (stack_top == NULL || stack_top->n != tortoise->n)
 			return (0);
-		listint_t *temp = stack_top;
-
+		temp = stack_top;
 		stack_top = stack_top->next;
 		free(temp);
 		tortoise = tortoise->next;
