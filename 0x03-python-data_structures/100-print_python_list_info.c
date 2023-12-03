4c682e02f0f1b75b8b818f4436144e9f54b58f65
RHEZUS <ludivinseunda2@gmail.com>
2023-12-03 14:24:17 +0300

    solve the mandatory tasks

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <Python.h>
 
+/**
+ * print_python_list_info - prints python list info
+ *
+ * @p: PyObject
+ * Return: no return
+ */
+
 void print_python_list_info(PyObject *p)
 {
-    int size, i;
-    long int size, i;
+	int size, i;
+	long int size, i;
 	PyListObject *list;
 	PyObject *item;
 
