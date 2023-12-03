8f413908eba7d8bb877963e1a3df5fc2af1febae
RHEZUS <ludivinseunda2@gmail.com>
2023-12-03 11:58:28 +0300

    solve the mandatory tasks

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(matrix):
         for j in range(matrix[i]):
             if (j != 0):
-                print("{:d}".format(j), end=" ")
-            else:
-                print("{:d}".format(j), end="\n")
+                print(" ", end="")
+            print("{:d}".format(matrix[i][j]), end="\n")
+        print()
