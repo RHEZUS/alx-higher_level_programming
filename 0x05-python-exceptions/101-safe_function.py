9623b32ff90fcef69ce8017a1b042516b674bd68
RHEZUS <ludivinseunda2@gmail.com>
2023-12-18 15:40:05 +0300

    solve tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+
+    try:
+        return fct(args[0], args[1])
+    except Exception as er:
+        print('Exception:', er, file=sys.stderr)
+        return None
