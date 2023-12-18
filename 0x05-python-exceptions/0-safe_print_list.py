921e92349639a288016087b92abbe0d5a563b010
RHEZUS <ludivinseunda2@gmail.com>
2023-12-18 15:48:41 +0300

    solve tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
             count = count + 1
-    except:     
+    except:
         print()
         return count
-    else:    
+    else:
         print()
         return count
