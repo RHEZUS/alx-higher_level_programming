bfa8ca34eb085b11665f81af869504835caa902e
RHEZUS <ludivinseunda2@gmail.com>
2023-12-18 16:00:48 +0300

    solve tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-
 def safe_function(fct, *args):
-
     try:
-        return fct(args[0], args[1])
+        result = fct(args[0], args[1])
     except Exception as er:
         print('Exception:', er, file=sys.stderr)
         return None
+    return result
