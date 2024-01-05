0198882539d82f12116b93ac7bf312d3d3926ab0
RHEZUS <ludivinseunda2@gmail.com>
2024-01-05 02:43:14 -0800

    Taks 102

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,27 @@
+#include "Python.h"
+
+/**
+ * print_python_string: prints Python strings.
+ * @p: the python object
+ * Return: nothing
+ */
+
+
+void print_python_string(PyObject *p)
+{
+	long int length = 0;
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("p is not a valid string");
+		return;
+	}
+
+	length = ((PyASCIIObject *)p)->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
