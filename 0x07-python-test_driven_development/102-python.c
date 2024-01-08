bfa230f0c7f6d816ed9ff50323b489ec627668a4
RHEZUS <ludivinseunda2@gmail.com>
2024-01-08 12:53:00 +0300

    solve tasks

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,11 +1,9 @@
 #include "Python.h"
 
 /**
- * print_python_string - prints Python strings.
- * @p: the python object
- * Return: nothing
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
  */
-
 void print_python_string(PyObject *p)
 {
 	long int length;
@@ -26,5 +24,5 @@ void print_python_string(PyObject *p)
 	else
 		printf("  type: compact unicode object\n");
 	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length))
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
