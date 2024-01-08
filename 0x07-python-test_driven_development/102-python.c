42f024bef79119874e505adab5376bc35cf03688
RHEZUS <ludivinseunda2@gmail.com>
2024-01-08 12:51:25 +0300

    solve tasks

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -2,28 +2,30 @@
 #include "Python.h"
 
 /**
- * print_python_string: prints Python strings.
+ * print_python_string - prints Python strings.
  * @p: the python object
  * Return: nothing
  */
 
-
 void print_python_string(PyObject *p)
 {
-    int length = 0;
-    if (strcmp(p->ob_type->tp_name, "str") != 0)
-    {
-        printf("p is not a valid string");
-        return;
-    }
+	long int length;
 
-    length = ((PyASCIIObject *)p)->length;
+	fflush(stdout);
 
-    if (PyUnicode_IS_COMPACT_ASCII(p))
-        print("[.] string object info")
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
 	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length))
 }
