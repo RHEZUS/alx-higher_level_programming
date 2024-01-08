d95e8e8f69fe86d98a360e142e9961c9214d5834
RHEZUS <ludivinseunda2@gmail.com>
2024-01-08 12:04:24 +0300

    solve tasks

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,6 +3,8 @@
 This is the "4-print_square" module
 The 4-print_square module supplies one function, print_square(size).
 """
+
+
 def print_square(size):
     """Prints a square"""
     if type(size) is not int:
