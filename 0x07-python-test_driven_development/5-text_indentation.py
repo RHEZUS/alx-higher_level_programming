a0338c1a5615c9fb75fe29be0664b9f2477e7a4f
RHEZUS <ludivinseunda2@gmail.com>
2024-01-08 12:16:46 +0300

    solve tasks

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,7 +7,7 @@ The 5-text_indentation module supplies one function, text_indentation(text).
 def text_indentation(text):
     """Indents a text"""
     result = []
-    separator = ("?", ".", ":", '\n')
+    separator = ("?", ".", ":")
     new_word = ""
     if type(text) is not str:
         raise TypeError('text must be a string')
@@ -21,4 +21,16 @@ def text_indentation(text):
                 new_word += ch
                 print("{}\n".format(new_word))
                 new_word = ""
-    
+                continue
+        
+    if new_word != "":
+            print(new_word)
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
