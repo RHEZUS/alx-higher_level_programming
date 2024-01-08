e6f41e582f748da27e129cf0b04aa60f95624acb
RHEZUS <ludivinseunda2@gmail.com>
2024-01-08 11:18:03 +0300

    solve tasks

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,5 +10,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif type(b) not in (int, float):
         raise TypeError('b must be an integer')
-
-    return int(a) + int(b)
+    int(a) + int(b)
+    return a + b
--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include "python3.11/Python.h"
 
+void print_python_string(PyObject *p)
+{
+    int length = 0;
+    if (strcmp(p->ob_type->tp_name, "str") != 0)
+    {
+        printf("p is not a valid string");
+        return;
+    }
+
+    length = ((PyASCIIObject *)p)->length;
+
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
