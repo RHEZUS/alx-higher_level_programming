6780b87b32ce61658da3b4d9ae1bf0c9c87bfb14
RHEZUS <ludivinseunda2@gmail.com>
2024-01-09 14:33:20 +0300

    solve tasks

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+contains the function load_from_json_file
+"""
+
+import json
+
+
 def load_from_json_file(filename):
-    pass
+    """reads objects from json file"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
