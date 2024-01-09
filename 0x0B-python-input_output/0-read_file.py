f31e665fdcd3f77c833aba8adc290d9526a0fda5
RHEZUS <ludivinseunda2@gmail.com>
2024-01-09 13:23:24 +0300

    solve tasks

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Contains the function read_file
+"""
+def read_file(filename=""):
+    """Reads a file and prints its content"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = f.read()
+        print(data)
