15c3dea56e39cbb25add226b7f2e93a31f8ee26d
RHEZUS <ludivinseunda2@gmail.com>
2024-01-09 13:46:35 +0300

    solve tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ contains the function write_file
 def write_file(filename="", text=""):
     """override a file or  create new one"""
     with open(filename, 'w', encoding='utf-8') as f:
-       return f.write(text)
+        return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+contains the function append_write
+"""
+
+
 def append_write(filename="", text=""):
-    pass
+    """appends  a text at the end of a file"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
