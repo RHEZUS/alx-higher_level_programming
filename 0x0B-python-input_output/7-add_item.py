c10f7166139bf5683108e79a4370b620f1bc271b
RHEZUS <ludivinseunda2@gmail.com>
2024-01-09 15:01:18 +0300

    solve tasks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
-def class_to_json(obj):
-    pass
+import sys
+import os.path
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+my_list = []
+filename = 'add_item.json'
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
+
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+save_to_json_file(my_list, filename)
