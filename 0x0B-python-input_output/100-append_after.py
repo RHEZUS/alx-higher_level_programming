deefb9c39f1c01558d5c28d864feeebb07298576
RHEZUS <ludivinseunda2@gmail.com>
2024-01-09 16:58:45 +0300

    solve tasks

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,14 +8,14 @@ def append_after(filename="", search_string="", new_string=""):
     """appends "new_string" after a line containing
     "search_string" in "filename" """
     with open(filename, 'r', encoding='utf-8') as f:
-        lines = []
-        while 1:
-            line = f.readline()
-            if line == "":
-                break
-            lines.append(line)
+        lines = f.readlines()
+        newlines = []
+        for line in lines:
             if search_string in line:
-                lines.append(new_string)
-
-    with open('READEME.txt', 'w', encoding='utf-8') as output:
-        output.writelines(lines)
+                line = line.strip() + '\n'
+                newlines.append(line)
+                newlines.append(new_string + '\n')
+            else:
+                newlines.append(line.strip() + '\n')
+    with open(filename, 'w', encoding='utf-8') as output:
+        output.writelines(newlines)
