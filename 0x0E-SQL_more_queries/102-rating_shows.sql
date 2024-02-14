f9ea86ac42de590dff3caea4bb30375ab2ddb748
RHEZUS <ludivinseunda2@gmail.com>
2024-02-14 14:08:04 +0300

    solve tasks

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+--  lists all shows from hbtn_0d_tvshows_rate by their rating.
+USE hbtn_0d_tvshows_rate;
+
+SELECT tv_shows.title, SUM(tv_ratings.rating) AS rating_sum
+FROM tv_shows
+LEFT JOIN tv_ratings ON tv_shows.id = tv_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating_sum DESC;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, states.name, cities.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
