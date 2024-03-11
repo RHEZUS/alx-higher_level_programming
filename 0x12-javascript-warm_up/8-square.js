36769a65f56769cf91468a6da0a5552ce1db5edc
RHEZUS <ludivinseunda2@gmail.com>
2024-03-11 03:34:07 -0700

    style

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const { argv } = require('node:process');
 if (argv.length === 2 || isNaN(parseInt(argv[2]))) {
   console.log('Missing size');
 } else {
-  for (i = 0; i < parseInt(argv[2]); i++) {
+  for (let i = 0; i < parseInt(argv[2]); i++) {
     console.log('X'.repeat(parseInt(argv[2])));
   }
 }
