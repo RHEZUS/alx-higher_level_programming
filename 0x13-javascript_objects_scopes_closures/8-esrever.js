15f79fdcc2cd4d33e406ad944f2440e7a86a6f5f
RHEZUS <ludivinseunda2@gmail.com>
2024-03-12 12:26:24 +0300

    wef

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+    var reversedArray = [];
+    for (var i = list.length - 1; i >= 0; i--) {
+      reversedArray.push(list[i]);
+    }
+    list = reversedArray;
+    return list;
 };
