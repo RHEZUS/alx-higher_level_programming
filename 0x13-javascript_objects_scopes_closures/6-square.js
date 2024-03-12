9c3a8b30bddbdaa7cc5c4ed3c6f7c28c0af0cd9b
RHEZUS <ludivinseunda2@gmail.com>
2024-03-12 11:32:39 +0300

    done

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+    constructor(size) {
+        super(size, size);
+    };
+
+    charPrint(c)
+    {
+        let char = c ===  undefined ? 'X' : C;
+        for (let i = 0; i < this.height; i++) {
+            console.log(char.repeat(this.width));  
+        }
+    }
+}
+module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+
+exports.esrever = function (list){
+    return list.reverse();
+}
