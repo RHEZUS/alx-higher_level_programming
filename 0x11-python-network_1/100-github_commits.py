2b75f5a38333a2ba0f42ee4f5e30aafd1758fffd
RHEZUS <ludivinseunda2@gmail.com>
2024-03-28 23:22:43 +0300

    a

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
+"""lists the 10 most recent commits on a given GitHub repository.
 """
-Write a Python script that takes 2
-arguments in order to solve this challenge
-"""
-
 import sys
 import requests
 
