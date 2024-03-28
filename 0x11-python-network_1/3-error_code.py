bb0626157f3ca2cce75e68122f6cb5c1a162bc94
RHEZUS <ludivinseunda2@gmail.com>
2024-03-28 22:52:24 +0300

    a

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,7 @@
 
 """
 - takes in a URL
-- sends a request to the URL 
+- sends a request to the URL
 - displays the body of the response (decoded in utf-8)
 """
 
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,7 +3,7 @@
 """
 - takes in a URL
 - sends a request to the URL
-- displays the value of the variable 
+- displays the value of the variable
 - X-Request-Id in the response header
 """
 
